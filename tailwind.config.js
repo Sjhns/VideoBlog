63ec121002da1437d6141b1035a2eb060d63fda7
johnson <johnson@github.com>
2023-01-13 01:06:49 -0300

    🌈 style(tailwind.config): add cor personalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        mainBg :"#040506"
+      }
+    },
   },
   plugins: [],
 }
