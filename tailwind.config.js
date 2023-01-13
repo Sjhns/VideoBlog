27fe722a7cdbf333ebafba282b86c6d75540ea4a
johnson <johnson@github.com>
2023-01-13 01:44:53 -0300

    🔧 build(tailwind.config):  personalizado os tamanhos dos layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors:{
-        mainBg :"#040506"
-      }
+      colors: {
+        mainBg: "#040506",
+      },
+
+      screens: {
+        sm: "320px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
     },
   },
   plugins: [],
-}
+};
