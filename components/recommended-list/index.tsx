781afe3fbe22f157fadcc5892ceba9651cac1399
johnson <johnson@github.com>
2023-01-14 16:38:29 -0300

    🦄 refactor: tornando o componente dinâmico

--- components/recommended-list/index.tsx
+++ components/recommended-list/index.tsx
@@ -1,14 +1,15 @@
-export const RecommendedList = () => {
-  const urlImage =
-    "https://www.ufmt.br/ocs/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png";
-
+interface RecommendedListProps{
+  imgSrc: string
+  videoName: string
+}
+export const RecommendedList = ({ imgSrc, videoName}: RecommendedListProps) => {
   return (
-    <div className="flex items-center mb-2 cursor-pointer px-3 py-2 
-    hover:bg-gray-900 duration-200 ease-in-out
-    
-    ">
+    <div
+      className="flex items-center mb-2 cursor-pointer px-3 py-2 
+    hover:bg-gray-900 duration-200 ease-in-out"
+    >
       <img
-        src={urlImage}
+        src={imgSrc}
         alt=""
         className="sm:w-[60px] sm:h-[60px]
       lg:w-[140px] lg:h-[80px] rounded-lg object-cover
@@ -16,7 +17,8 @@ export const RecommendedList = () => {
       />
       <div className="ml-2 flex-1">
         <h3 className="text-textColor lg:text-[16px] sm:text-[12px]">
-          animal Kingdom <span className="block text-[12px] text-gray-400">animal</span>
+          {videoName}
+          <span className="block text-[12px] text-gray-400">animal</span>
         </h3>
         <div className="flex items-center mt-2">
           <p className="text-[14px] font-bold text-gray-500">40:30</p>
